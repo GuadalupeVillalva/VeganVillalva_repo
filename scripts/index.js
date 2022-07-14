85a7e7b1b14889b437ec2e3b23324a15a35e904a
Guadalupe Villalva <guadi_lupev@hotmail.com>
2022-07-13 23:11:02 -0300

    desafio-clase4

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,26 @@
+// Suma productos de una tienda, aplica IVA y luego aplica un cupón 
+// de descuento si este es ingresado.
 
-// Turnos para el mes de noviembre
-for (let i = 1; i <= 30; i++) {  
-    //solicita nombre
-    let ingresarNombre = prompt("¿Cómo te llamás?");
-    //solicita día del mes (1 al 30)
-    let ingresarNumero = prompt("¿Para que día del mes de noviembre desea su turno?");
-    //en caso de que se ingrese un número mayor a 30 se detiene
-    if (ingresarNumero >30){
-        break;
+//declaro las constantes de suma y resta
+const sumar = (a, b) => a + b;
+const restar = (a, b) => a - b;
+//declaro la constante de IVA
+const iva = a => a * 0.21;
+//declaro los productos
+let precioProducto1 = 213;
+let precioProducto2 = 547;
+//declaro el cupón
+let cupon = prompt("Ingresar cupón de descuento");
+//declaro la suma de los productos con IVA aplicado
+let sumaTotal = sumar(sumar(precioProducto1, iva(precioProducto1)), sumar(precioProducto2, iva(precioProducto2)));
+
+//funcion que calcula la suma total de los productos
+function precioTotal() {
+    if(cupon === "DESCUENTO"){
+        return restar(sumaTotal, 320); //si la palabra DESCUENTO es ingresada se aplica un cupón
+    } else {
+        return sumaTotal;
     }
-    //devuelve alerta
-    alert(ingresarNombre + " se le ha asignado el turno n°"+ i + " para el día "+ ingresarNumero + " de noviembre");
 }
+
+
